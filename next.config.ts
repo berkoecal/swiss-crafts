84a5290b58b35950d650b2b745a0dbd5ad0d989e
berkoecal <berkoecal@gmail.com>
2025-03-13 18:57:49 +0100

    Update Next.js configuration to include basePath and assetPrefix for repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName = "swiss-crafts";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
